722d625003c3edbafec98c6e70346195dda9be16
Matt Dalton <mathew.d.dalton@gmail.com>
2024-06-27 22:03:26 -0700

    Got better unit tests. Moved tests to ignore/ until ready to use

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,6 +1,6 @@
 class HTMLNode:
     
-    def __init__(self, tag, value, children, props):
+    def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
         self.children = children
